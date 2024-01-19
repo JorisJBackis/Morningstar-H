0575a22ae54305eb79ac40bf9bbfe4a6b62ea494
DerakhshanRadbareh <146732331+DerakhshanRadbareh@users.noreply.github.com>
2024-01-19 10:32:14 +0100

    Create dbh.php

---
+++ dbh.php
@@ -0,0 +1,11 @@
+$dsn="mysql:host=localhost;dbname=userDB";
+$dbusername="admin";
+$dbpassword="123456";
+
+
+try {
+    $pdo= new PDO ($dsn,$dbusername,$dbpassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " .$e ->getMessage();
+}
